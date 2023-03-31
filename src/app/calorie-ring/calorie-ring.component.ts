e4166e7758d8abbc2fd131856d1b9e3188d72b7c
jeff <jsmeendering@gmail.com>
2023-03-30 23:28:56 -0400

    feat: added a calorie-ring, main-page, navbar and weekly-chart.

---
+++ src/app/calorie-ring/calorie-ring.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-calorie-ring',
+  templateUrl: './calorie-ring.component.html',
+  styleUrls: ['./calorie-ring.component.css']
+})
+export class CalorieRingComponent {
+
+}
---
+++ src/app/weekly-chart/weekly-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-chart',
+  templateUrl: './weekly-chart.component.html',
+  styleUrls: ['./weekly-chart.component.css']
+})
+export class WeeklyChartComponent {
+
+}
